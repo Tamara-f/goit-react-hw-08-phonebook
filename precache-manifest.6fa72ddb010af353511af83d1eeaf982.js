ce33b942179a48b858608794b3ff11c273a2565e
tamara-f <filatovatam@ukr.net>
2020-10-30 14:51:02 +0200

    Updates

---
+++ precache-manifest.6fa72ddb010af353511af83d1eeaf982.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceae23510900c43068d590e14fdcc175",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "dd020c98883ace9f037c",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.f167bd6e.chunk.css"
+  },
+  {
+    "revision": "778f0591ae45f8afd3db",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.2c034a0b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.2c034a0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "448950573ab2af918ed2",
+    "url": "/goit-react-hw-08-phonebook/static/js/3.9e5be033.chunk.js"
+  },
+  {
+    "revision": "96f2a2c4369942223e41",
+    "url": "/goit-react-hw-08-phonebook/static/js/4.2825d02c.chunk.js"
+  },
+  {
+    "revision": "1f739451d607b941ec82",
+    "url": "/goit-react-hw-08-phonebook/static/js/5.d8706238.chunk.js"
+  },
+  {
+    "revision": "d81cf04d651447d8a573",
+    "url": "/goit-react-hw-08-phonebook/static/js/6.fa66ec60.chunk.js"
+  },
+  {
+    "revision": "3aa43958b153ddf1b361",
+    "url": "/goit-react-hw-08-phonebook/static/js/7.30ee06df.chunk.js"
+  },
+  {
+    "revision": "dd020c98883ace9f037c",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.b68a3bef.chunk.js"
+  },
+  {
+    "revision": "96b4fc73842c90dd9f4b",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.a67544cb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-08-phonebook/precache-manifest.e708cbc9031653e708ea0568cbc9014d.js"
+  "/goit-react-hw-08-phonebook/precache-manifest.6fa72ddb010af353511af83d1eeaf982.js"
 );
 
 self.addEventListener('message', (event) => {
